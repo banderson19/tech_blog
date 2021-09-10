038fa650739ea15f476962aec5dc810e83d40dcd
ogMav8 <brad.anderson019@gmail.com>
2021-09-10 14:05:22 -0600

    server

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 // turn on routes
 app.use(routes);
@@ -28,33 +26,4 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
-// // const sess = {
-// //   secret: 'Super secret secret',
-// //   cookie: {
-// //     expires: 10 * 60 * 1000
-// //   },
-// //   resave: false,
-// //   saveUninitialized: true,
-// //   store: new SequelizeStore({
-// //     db: sequelize
-// //   })
-// // };
 
-// // app.use(session(sess));
-
-// // const helpers = require('./utils/helpers');
-
-// // const hbs = exphbs.create({ helpers });
-
-// // app.engine('handlebars', hbs.engine);
-// // app.set('view engine', 'handlebars');
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // app.use(require('./controllers/'));
-
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening'));
-// });
