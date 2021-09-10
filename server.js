bd54765a73e8aebb3a0d0a2376935996f1511441
ogMav8 <brad.anderson019@gmail.com>
2021-09-09 23:02:48 -0600

    customized the dashboard

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // turn on routes
 app.use(routes);
@@ -16,18 +27,6 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
-
-// const path = require('path');
-// const express = require('express');
-// const session = require('express-session');
-// const exphbs = require('express-handlebars');
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // // const sess = {
 // //   secret: 'Super secret secret',
